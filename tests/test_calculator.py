01f8ccd886c939d072363940bb6d14d3bd78404c
root <root@AnthonyASUS>
2024-10-31 22:10:05 -0400

    added functions and test for multiplication and division

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,18 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator.operation_basic_function import add, subtract, mulitply, divide
 
 def test_addition():
     '''Test that addition function works '''    
     assert add(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
+    '''Test that subtraction function works '''    
     assert subtract(2,2) == 0
-    
+
+def test_multiplication():
+    '''Test that the multiplication function works'''
+    assert mulitply(5,2) == 10
+
+def test_division():
+    '''Test that the division funtion works'''
+    assert divide(10,2) == 5
